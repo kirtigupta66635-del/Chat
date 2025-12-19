0f6d8e93e4039502003962570914f85fda32fc8e
kirtigupta66635-del <kirtigupta66635@gmail.com>
2025-12-19 22:22:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
-OWNER_ID = 123456789
-DB_NAME = "chatgame.db"
+BOT_TOKEN = "7789010028:AAEA5dC6R99-nHRR1KPlXCl74KvlTjTx6zo"
+OWNER_ID = 7995588921
+DB_NAME = "mongodb+srv://h17589479_db_user:W5l1NuSQ2cak8I04@cluster0.tkxlnpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 START_EMOJI = "🎮🤖"
 GAME_TIME_START = 20
