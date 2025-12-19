de53163a85a10a52c5c0ceebea5e2f453ef00acf
kirtigupta66635-del <kirtigupta66635@gmail.com>
2025-12-19 22:29:43 +0530

    Delete Aditya/Model.py directory

--- Aditya/Model.py/Models.py
+++
@@ -1,8 +0,0 @@
-# simple behavior model placeholder
-
-def mood(text):
-    if "sad" in text.lower():
-        return "😢 Don't worry, I'm here"
-    if "happy" in text.lower():
-        return "😄 That's great!"
-    return "🤖 Hmm..."
