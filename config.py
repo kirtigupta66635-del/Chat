b09d8f3ac053fef21f073840decdf0c34f99f04d
kirtigupta66635-del <kirtigupta66635@gmail.com>
2025-12-20 13:00:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,11 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7789010028:AAEA5dC6R99-nHRR1KPlXCl74KvlTjTx6zo")
+OWNER_ID = int(os.getenv("OWNER_ID", "7995588921"))
 
-# MongoDB Atlas URI
-MONGO_URI = os.getenv(
-    "MONGO_URI",
+# 🔴 यही line missing है
+MONGO_URL = os.getenv(
+    "MONGO_URL",
     "mongodb+srv://h17589479_db_user:W5l1NuSQ2cak8I04@cluster0.tkxlnpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 )
 
