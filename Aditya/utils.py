2b56ada704d15e0d590e3f71837b63e8e3cc2fcb
kirtigupta66635-del <kirtigupta66635@gmail.com>
2025-12-20 14:23:25 +0530

    Update utils.py

--- Aditya/utils.py
+++ Aditya/utils.py
@@ -1,14 +1,30 @@
 import random
+from wordfreq import zipf_frequency
 
-WORDS = {
-    "animal": ["CAT", "LION", "TIGER", "DOG"],
-    "human": ["KING", "QUEEN", "DOCTOR"],
-    "history": ["ROME", "INDIA"]
-}
+USED_WORDS = set()
 
-def masked_word():
-    category = random.choice(list(WORDS.keys()))
-    word = random.choice(WORDS[category])
-    idx = random.randint(1, len(word)-2)
-    masked = word[:idx] + "_" + word[idx+1:]
-    return masked, word
+def get_random_word(min_len, max_len=None):
+    while True:
+        length = random.randint(min_len, max_len or min_len + 3)
+        word = "".join(random.choice("abcdefghijklmnopqrstuvwxyz") for _ in range(length))
+        if zipf_frequency(word, "en") > 2.5 and word.upper() not in USED_WORDS:
+            USED_WORDS.add(word.upper())
+            return word.upper()
+
+def pick_word_by_level(level):
+    if level < 10:
+        return get_random_word(3,5)
+    elif level < 20:
+        return get_random_word(6,8)
+    else:
+        return get_random_word(9,12)
+
+def mask_word(word):
+    masked = ""
+    for c in word:
+        masked += "_" if random.random() < 0.6 else c
+    return masked
+
+def masked_word(level):
+    word = pick_word_by_level(level)
+    return mask_word(word), word
