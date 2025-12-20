47d8cd2f7615f24ed23596d4724701ff0968a7a5
kirtigupta66635-del <kirtigupta66635@gmail.com>
2025-12-20 12:44:31 +0530

    Rename DB_NAME to MONGO_URL in config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 BOT_TOKEN = "7789010028:AAEA5dC6R99-nHRR1KPlXCl74KvlTjTx6zo"
 OWNER_ID = 7995588921
-DB_NAME = "mongodb+srv://h17589479_db_user:W5l1NuSQ2cak8I04@cluster0.tkxlnpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URL = "mongodb+srv://h17589479_db_user:W5l1NuSQ2cak8I04@cluster0.tkxlnpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 START_EMOJI = "🎮🤖"
 GAME_TIME_START = 20
