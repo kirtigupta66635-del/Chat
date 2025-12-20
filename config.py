30cc625d602f0225c6cb54740ea46861f9f1b6f6
kirtigupta66635-del <kirtigupta66635@gmail.com>
2025-12-20 18:36:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7789010028:AAEA5dC6R99-nHRR1KPlXCl74KvlTjTx6zo")
 OWNER_ID = int(os.getenv("OWNER_ID", "7995588921"))
-
+# OpenAI API Key
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
 # 🔴 यही line missing है
 MONGO_URL = os.getenv(
     "MONGO_URL",
